9574ee792b42f03261e9c03644c3137c8d63cae8
Ivan Torrez <itorrez1506@gmail.com>
2024-03-31 01:00:12 -0400

    config React Query

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,19 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import theme from "./theme/theme";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ChakraProvider theme={theme}>
     <Router>
       <React.StrictMode>
         <ColorModeScript />
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools />
+          <App />
+        </QueryClientProvider>
       </React.StrictMode>
     </Router>
   </ChakraProvider>
